b68562b8b20c4e27631eb44be351d5d293e3d393
srsviegas <rudiero.viegas@gmail.com>
2023-04-19 11:51:50 -0300

    DOC: Add description to `__init__`

--- pyproplogic/__init__.py
+++ pyproplogic/__init__.py
@@ -1,5 +1,12 @@
+"""
+PyPropLogic
+===========
+Python library for propositional logic formulas parsing, manipulation and visualization.
+"""
+
 from .logic_formula import LogicFormula
 from .random_formula import random_formula
+
 
 __title__ = "pyproplogic"
 __author__ = "srsviegas"
